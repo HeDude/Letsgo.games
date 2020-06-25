2c0fd7f1c4e56e6ab8281a48fee1ce22aea51d43
Bijbrengen <slagter@bijbrengen.nl>
2020-06-25 07:58:29 +0200

    Error logging

--- index.php
+++ index.php
@@ -31,13 +31,20 @@ $game_locations = array
    5 => array( 'path' => 'russian/a/5/', 'type' => 'jpg' ),
    6 => array( 'path' => 'russian/a/6/', 'type' => 'jpg' ),
    7 => array( 'path' => 'russian/a/7/', 'type' => 'jpg' ),
-   8 => array( 'path' => 'russian/c/1/', 'type' => 'jpg' ),
-   9 => array( 'path' => 'russian/c/2/', 'type' => 'jpg' ),
-  10 => array( 'path' => 'russian/c/3/', 'type' => 'jpg' ),
-  11 => array( 'path' => 'russian/c/4/', 'type' => 'jpg' ),
-  12 => array( 'path' => 'russian/c/5/', 'type' => 'jpg' ),
-  13 => array( 'path' => 'russian/c/6/', 'type' => 'jpg' ),
-  14 => array( 'path' => 'russian/c/7/', 'type' => 'jpg' )
+   8 => array( 'path' => 'russian/b/1/', 'type' => 'png' ),
+   9 => array( 'path' => 'russian/b/2/', 'type' => 'png' ),
+  10 => array( 'path' => 'russian/b/3/', 'type' => 'png' ),
+  11 => array( 'path' => 'russian/b/4/', 'type' => 'png' ),
+  12 => array( 'path' => 'russian/b/5/', 'type' => 'png' ),
+  13 => array( 'path' => 'russian/b/6/', 'type' => 'png' ),
+  14 => array( 'path' => 'russian/b/7/', 'type' => 'png' ),
+  15 => array( 'path' => 'russian/c/1/', 'type' => 'jpg' ),
+  16 => array( 'path' => 'russian/c/2/', 'type' => 'jpg' ),
+  17 => array( 'path' => 'russian/c/3/', 'type' => 'jpg' ),
+  18 => array( 'path' => 'russian/c/4/', 'type' => 'jpg' ),
+  19 => array( 'path' => 'russian/c/5/', 'type' => 'jpg' ),
+  20 => array( 'path' => 'russian/c/6/', 'type' => 'jpg' ),
+  21 => array( 'path' => 'russian/c/7/', 'type' => 'jpg' )
 );
 if ( !array_key_exists( $game_number, $game_locations ) )
 {
